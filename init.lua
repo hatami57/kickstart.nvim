0eaffefea6156cbae5e8556bc8e607f979c7dd68
Majid Hatami <hatami6570@gmail.com>
2025-07-22 10:49:14 +0330

    chore: set C-l as Left key in insert mode instead of ^L char

--- init.lua
+++ init.lua
@@ -1047,5 +1047,7 @@ require('lazy').setup({
 -- vim.o.tabstop = 2
 -- vim.o.shiftwidth = 2
 
+vim.keymap.set('i', '<C-l>', '<Right>')
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
