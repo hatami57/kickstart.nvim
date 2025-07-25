2dbb1a0ab2a690ae92a730ab96c633ef0f3c0398
Majid Hatami <hatami6570@gmail.com>
2025-07-25 11:16:37 +0330

    chore: set tab size and smart indent etc.

--- init.lua
+++ init.lua
@@ -1042,10 +1042,10 @@ require('lazy').setup({
   },
 })
 
--- vim.o.expandtab = true
--- vim.o.smartindent = true
--- vim.o.tabstop = 2
--- vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 
 vim.keymap.set('i', '<C-l>', '<Right>')
 
