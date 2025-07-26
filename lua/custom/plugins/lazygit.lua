257f2421bda0ad4bdf9ab688db125412b1563f20
Majid Hatami <hatami6570@gmail.com>
2025-07-26 11:33:45 +0330

    chore: add support for telescope for lazygit

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -10,11 +10,16 @@ return {
   },
   -- optional for floating window border decoration
   dependencies = {
+    'nvim-telescope/telescope.nvim',
     'nvim-lua/plenary.nvim',
   },
+  config = function()
+    require('telescope').load_extension 'lazygit'
+  end,
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
     { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    { '<leader>slg', '<cmd>lua require("telescope").extensions.lazygit.lazygit()<cr>', desc = 'LazyGit' },
   },
 }
